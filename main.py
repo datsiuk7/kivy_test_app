8b75688fe6daea76da006d325c0c5aca70665b8b
datsiuk7 <datcukbogdan@gmail.com>
2024-02-02 14:22:56 +0200

    fix

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
-
-from kivy.app import App
-from kivy.uix.label import Label
-
-
-class MyApp(App):
-
+from kivymd.app import MDApp
+# from kivy.lang import Builder
+# Builder.load_file('home.kv')
+class ConverterApp(MDApp):
+    def on_start(self):
+        super().on_start()
+        # self.root.ids.tabs.switch_tab(icon="home")
+        # self.root.ids.tabs.set_active_item(self.root.ids.tabs.get_tabs_list()[2])  # Встановити другу вкладку (індекс 1) як активну
+        # print("-----------------")
+        # print(dir(self.root.ids.tabs))
+        # print("-----------------")
+        # print(self.root.ids.tabs.get_tabs_list())
+        # self.root.ids.tabs.set_current_tab(self.root.ids.tab_home)
+    def go(self):
+        print("привіт")
     def build(self):
-        return Label(text='Hello world')
+        # self.theme_cls.theme_style = "Dark"
+        return
 
 
-if __name__ == '__main__':
-    MyApp().run()
+ConverterApp().run()
